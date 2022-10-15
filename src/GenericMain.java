defe9a3d95cf14a4dcc7ca6c1bf61ee9f51e06c0
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-15 21:06:43 +0530

    first commit

---
+++ src/GenericMain.java
@@ -0,0 +1,21 @@
+public class GenericMain {
+    public static <T extends Comparable<T>> T maximum(T x,T y,T z){
+        T max = x;
+
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+
+        return max;
+    }
+
+    public static void main(String args[]){
+        //Test case 1.1-Given Max Number at 1st Position return the Same Number
+        System.out.printf("Maximum of %d,%d and %d is %d\n\n", 5,4,3,maximum(3,4,5));
+        //Test case 1.2-Given Max Number at 2nd Position return the Same Number
+        System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,5,4,maximum(3,4,5));
+        //Test case 1.3-Given Max Number at 3rd Position return the Same Number
+        System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,4,5,maximum(3,4,5));
+    }
+}
