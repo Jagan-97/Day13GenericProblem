67645a89ed4dfd528bd6cd13de7196ddfec956e0
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-17 06:20:08 +0530

    Added FloatMax logic

--- src/GenericMain.java
+++ src/GenericMain.java
@@ -17,5 +17,12 @@ public class GenericMain {
         System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,5,4,maximum(3,5,4));
         //Test case 1.3-Given Max Number at 3rd Position return the Same Number
         System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,4,5,maximum(3,4,5));
+        //Given 3 Float Number
+        //Test case 1.1-Given Max Number at 1st Position return the Same Number
+        System.out.printf("Maximum of %.1f,%.1f and %.1f is %.1f\n\n", 8.8,7.7,6.6,maximum(8.8,7.7,6.6));
+        //Test case 1.2-Given Max Number at 2nd Position return the Same Number
+        System.out.printf("Maximum of %.1f,%.1f and %.1f is %.1f\n\n", 7.7,8.8,6.6,maximum(7.7,8.8,6.6));
+        //Test case 1.3-Given Max Number at 3rd Position return the Same Number
+        System.out.printf("Maximum of %.1f,%.1f and %.1f is %.1f\n\n", 6.6,7.7,8.8,maximum(6.6,7.7,8.8));
     }
 }
