c3eff4ae69b519f102868aa5391172cdfcc0b760
Jagan-97 <jagan.psaran38@gmail.com>
2022-10-17 06:01:56 +0530

    added IntegerMax logic

--- src/GenericMain.java
+++ src/GenericMain.java
@@ -16,6 +16,6 @@ public class GenericMain {
         //Test case 1.2-Given Max Number at 2nd Position return the Same Number
         System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,5,4,maximum(3,5,4));
         //Test case 1.3-Given Max Number at 3rd Position return the Same Number
-        System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,4,5,maximum(3,4,5))
+        System.out.printf("Maximum of %d,%d and %d is %d\n\n", 3,4,5,maximum(3,4,5));
     }
 }
